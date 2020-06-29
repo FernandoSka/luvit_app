76851052a879c54c9c08d889a97d8dc8124ee150
FernandoSka <yayo280.fl@gmail.com>
2020-06-28 22:40:41 -0500

    created product models

--- API/models.py
+++ API/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from .validators import min_value_validator, min_name_length_validator
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=55, validators=[min_name_length_validator], null=False, blank=False)
+    value = models.FloatField(null=False, blank=False, validators=[min_value_validator])
+    stock = models.IntegerField(null=False, blank=False, validators=[min_value_validator])
+    discount_value = models.FloatField(null=True, blank=True, default=0)
+
---
+++ API/validators.py
@@ -0,0 +1,19 @@
+from django.core.exceptions import ValidationError
+
+def min_value_validator(value):
+    if value < 0:
+        raise ValidationError(
+            _('The value should be up to 0'),
+            params={'value': value},
+        )
+
+def min_name_length_validator(value):
+    if not 3 < len(value) < 55:
+        raise ValidationError(
+            _('The product name should have characters between 3 and 55'),
+            params={
+                'value': value,
+                'length': len(value)
+            },
+        )
+
